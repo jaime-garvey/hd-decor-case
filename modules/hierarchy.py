08ce5044c656790ccdc5ee45582c52efee34a8af
Jaime <jaimemgarvey@gmail.com>
2019-07-18 11:34:05 -0700

    add hierarchy.py

--- modules/hierarchy.py
+++ modules/hierarchy.py
@@ -1,76 +1,84 @@
 import pandas as pd
 import numpy as np
-
 from sklearn import preprocessing
 
-class Tree:
+'''
+Build product hierarchy dictionary.
+'''
+
+
+class Hierarchy:
     '''
     Build parent, child dictionary 
     '''
+    
     def __init__(self, data, num_levels):
         '''
-        
         Keyword Arguments:
         ------------------
-        
+        * data - flattened pandas dataframe
+        * num_levels - number of product category levels
         '''
+        
         self.data= data
         self.num_levels = num_levels
-        self.level_cols = data.columns[-self.num_levels]
+        self.level_cols = list(data.columns[-self.num_levels:])
         self.was_fit=False
         
-    def fit(self):
+        
+    def fit_transform(self):
+        '''
+        Fit (Encode Notes) and Transform (Categories to Ids in Dataframe)
+        '''
         # get notes
-        self.nodes = pd.unique(self.data.iloc[:,-self.num_levels].values.ravel())
+        self.nodes = list(pd.unique(self.data.iloc[:,-self.num_levels:].values.ravel()))
         
         #encode nodes
         le = preprocessing.LabelEncoder()
-        le.fit(list(self.nodes))
+        le.fit(self.nodes)
         
         #save mapping
         self.node2id = dict(zip(le.classes_, le.transform(le.classes_)))
         self.id2node = dict(zip(le.transform(le.classes_),le.classes_))
         
+        self.data_encoded = self.data
+        
+        for col in self.level_cols:
+            self.data_encoded[col] = pd.Series(le.transform(self.data_encoded[col].to_list()))
+            
         return self
+    
         
     def get_mappings(self, map_direction='encode'):
         '''
-        Get mapping for nodes and ids
+        Get mapping for nodes and ids (and ids to nodes)
+        
+        Returns:
+        --------
+        dictionary
         '''
+        
         if map_direction == 'encode':
             return self.node2id
-        elif map_direct == 'decode':
+        elif map_direction == 'decode':
             return self.id2node
-        else: 
-            return self.node2id, self.id2node
-        
-        
-    def transform(self):
-        '''
-        '''
-        if not self.was_fit:
-            raise Error("Need to fit data first")
-            
-        data_new = self.data
-        for col in self.cols:
-            data_new[col] = pd.Series(le.transform(data_new[col].to_list()))
-            
-        return self.data_new
-            
-        
-    def fit_transform(self):
-        '''
-        '''
-        return self.fit().transform()
-        
+
     
     def get_parent_child_dict(self):
         '''
+        Construct encoded parent - child dictionary
+        (e.g. {2: [5,9,22]...})
+        
+        Returns:
+        --------
+        dictionary
         '''
+         
+        # Function to group category levels with sliding window 
         def parent_child_dict(col_lst):
             for i in range(len(col_lst)-1):
                 j=i+1
-                yield(self.data_new.groupby(col_lst[i])[col_lst[j]].apply(set).to_dict())
+                yield(self.data_encoded.groupby(col_lst[i])[col_lst[j]].apply(set).to_dict())
             
         level_dicts = list(parent_child_dict(self.level_cols))
         
@@ -81,7 +89,3 @@ class Tree:
         self.prod_map = level_dicts[0]
         
         return self.prod_map
-        
-        
-        
-            
